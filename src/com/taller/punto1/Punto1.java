12f6c11d294485f35373f209057661b3b25af23a
jhon mc <jhoneder@hotmail.com>
2021-02-15 16:35:54 -0500

    Se crea metodo estatico para realizar el punto1

---
+++ src/com/taller/punto1/Punto1.java
@@ -0,0 +1,28 @@
+package com.taller.punto1;
+
+
+import com.taller.teclado.Teclado;
+
+import java.time.temporal.Temporal;
+
+public class Punto1 {
+
+
+    private static final String MENAJE_MAYOR="El numero mayor es: ";
+    private static final String MENAJE_SON_IGUALES="Los numeros son iguales ";
+
+    public  static void numeroMayor(Double numero1, Double numero2){
+        if(numero1 > numero2){
+            System.out.println(MENAJE_MAYOR+numero1);
+        }else if (numero1 < numero2){
+            System.out.println(MENAJE_MAYOR+numero2);
+        }else {
+            System.out.println(MENAJE_SON_IGUALES);
+        }
+    }
+
+    public static void main(String[] args) {
+        Punto1.numeroMayor(10.2, 20.2);
+    }
+
+}
