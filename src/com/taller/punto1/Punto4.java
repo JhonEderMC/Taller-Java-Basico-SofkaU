bd1381c8687e92d2b79ffc89388ab47b414210c7
jhon mc <jhoneder@hotmail.com>
2021-02-15 17:35:33 -0500

    Se refactoriza en Teclado metodo entrada por entradaTexto y se crea entradaDobuble.
    Se hace el Punto4

---
+++ src/com/taller/punto1/Punto4.java
@@ -0,0 +1,17 @@
+package com.taller.punto1;
+
+import com.taller.teclado.Teclado;
+
+public class Punto4 {
+
+    public static void calcularIva21(Double dinero){
+        dinero = dinero + dinero*0.21;
+        System.out.println("El precio con el 21% de iva es: "+dinero);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese la cantidad de dinero a calcular el IVA: ");
+        Double dinero = Teclado.entradaDouble();
+        Punto4.calcularIva21(dinero);
+    }
+}
--- src/com/taller/teclado/Teclado.java
+++ src/com/taller/teclado/Teclado.java
@@ -7,9 +7,20 @@ import java.util.Scanner;
  */
 public class Teclado {
 
-    public static String entrada(){
+    public static String entradaTexto(){
         Scanner scanner = new Scanner(System.in);
         return  scanner.nextLine();
+    }
+
+    public static Double entradaDouble(){
+        Scanner scanner = new Scanner(System.in);
+        Double number=0.0;
+        try{
+            number = scanner.nextDouble();
+        }catch (NumberFormatException e){
+            System.out.println("Por favor ingrese un numero separado por punto");
+        }
+    return number;
     }
 
 }
