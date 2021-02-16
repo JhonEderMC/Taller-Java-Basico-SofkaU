0523acda88be8c61f7226200075ed375b50d4ad5
jhon mc <jhoneder@hotmail.com>
2021-02-15 20:20:00 -0500

    Se creao el metodo reemplazarEspaciosBlanc en Punto10

---
+++ src/com/taller/punto1/Punto10.java
@@ -0,0 +1,17 @@
+package com.taller.punto1;
+
+import com.taller.teclado.Teclado;
+
+public class Punto10 {
+
+    public static void reemplazarEspaciosBlanc(String text){
+        text = text.replace(' ', '-');
+        System.out.println("Texto sin espacios en blanco:\n"+text);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese un texto: ");
+        String text = Teclado.entradaTexto();
+        Punto10.reemplazarEspaciosBlanc(text);
+    }
+}
