3bd2e454e647ec0a24c57ec00bcda187b1b0e6da
jhon mc <jhoneder@hotmail.com>
2021-02-15 20:39:38 -0500

    Se creao el metodo contarVocales en Punto11

---
+++ src/com/taller/punto1/Punto11.java
@@ -0,0 +1,51 @@
+package com.taller.punto1;
+
+import com.taller.teclado.Teclado;
+
+import javax.security.auth.callback.TextOutputCallback;
+
+public class Punto11 {
+
+    public static void contarVocales(String texto){
+        int countA=0,countE=0, countI=0,countO=0, countU=0;
+        System.out.println("La longitud del string es: "+texto.length());
+        texto = texto.toLowerCase(); //minuscula todo.
+        for (int i = 0; i <texto.length() ; i++) {
+            char v=texto.charAt(i);
+            if(v == 'a'){
+                countA++;
+            }else if(v =='e'){
+                countE++;
+            }else if(v=='i'){
+                countI++;
+            }else if (v =='o'){
+                countO++;
+            }else if(v == 'u'){
+                countU++;
+            }
+        }
+        System.out.println("a: "+countA);
+        System.out.println("e: "+countE);
+        System.out.println("i: "+countI);
+        System.out.println("o: "+countO);
+        System.out.println("u: "+countU);
+    }
+
+    public void countVocal(char v){
+        switch (v){
+            case 'a':
+                //countA++;
+                break;
+            case 'b':
+                //countA++;
+                break;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Ingrese una frase: ");
+        String texto= Teclado.entradaTexto();
+        Punto11.contarVocales(texto);
+    }
+}
