9204dd1e086415cb6f90703626cac14a52798be0
jhon mc <jhoneder@hotmail.com>
2021-02-18 21:34:17 -0500

    Se implementa la interfaz entregable en las clase Video  y Serie.
     Se implementan todos los metodos asociados en cada una de las clases.

--- src/com/taller/punto18/Serie.java
+++ src/com/taller/punto18/Serie.java
@@ -1,6 +1,6 @@
 package com.taller.punto18;
 
-public class Serie {
+public class Serie implements Entregable {
 
     private static final Integer TEMPORADAS_POR_DEFECTO=3;
     private static final Boolean ENTREGADO_POR_DEFECTO=Boolean.FALSE;
@@ -27,6 +27,26 @@ public class Serie {
         this.numeroTemporados = numeroTemporados;
         this.entregado = entregado;
         this.genero = genero;
+    }
+
+    @Override
+    public void entregar() {
+        this.entregado=Boolean.TRUE;
+    }
+
+    @Override
+    public void devolver() {
+        this.entregado=Boolean.FALSE;
+    }
+
+    @Override
+    public Boolean isEntregado() {
+        return this.entregado;
+    }
+
+    @Override
+    public void compareTo(Object a) {
+
     }
 
     public String getTitulo() {
--- src/com/taller/punto18/VideoJuego.java
+++ src/com/taller/punto18/VideoJuego.java
@@ -1,6 +1,6 @@
 package com.taller.punto18;
 
-public class VideoJuego {
+public class VideoJuego  implements  Entregable{
 
     private static final Integer HORAS_ESTIMADAS_POR_DEFECTO=10;
     private static final Boolean ENTREGADO_POR_DEFECTO=Boolean.FALSE;
@@ -26,6 +26,26 @@ public class VideoJuego {
         this(titulo, horasEstimadas);
         this.genero = genero;
         this.compania = compania;
+    }
+
+    @Override
+    public void entregar() {
+        this.entregado=Boolean.TRUE;
+    }
+
+    @Override
+    public void devolver() {
+        this.entregado=Boolean.FALSE;
+    }
+
+    @Override
+    public Boolean isEntregado() {
+        return this.entregado;
+    }
+
+    @Override
+    public void compareTo(Object a) {
+
     }
 
     public String getTitulo() {
